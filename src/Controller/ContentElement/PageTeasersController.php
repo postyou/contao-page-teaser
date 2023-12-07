f503fd466655c360e463c7642428acb18c24ece9
MarkejN <markus.nestmann@outlook.com>
2023-12-07 13:41:59 +0100

    Use the custom template only in the frontend

--- src/Controller/ContentElement/PageTeasersController.php
+++ src/Controller/ContentElement/PageTeasersController.php
@@ -7,21 +7,24 @@ namespace Postyou\ContaoPageTeaserBundle\Controller\ContentElement;
 use Contao\ContentModel;
 use Contao\CoreBundle\Controller\ContentElement\AbstractContentElementController;
 use Contao\CoreBundle\DependencyInjection\Attribute\AsContentElement;
+use Contao\CoreBundle\Routing\ScopeMatcher;
 use Contao\Model\Collection;
 use Contao\PageModel;
 use Contao\StringUtil;
 use Contao\Template;
 use Postyou\ContaoPageTeaserBundle\Service\PageTeaserService;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 
-#[AsContentElement(category: 'links',template: 'ce_page_teasers')]
+#[AsContentElement(category: 'links', template: 'ce_page_teasers')]
 class PageTeasersController extends AbstractContentElementController
 {
     public function __construct(
         protected readonly PageTeaserService $pageTeaserService,
-    ) {
-    }
+        protected readonly RequestStack $requestStack,
+        protected readonly ScopeMatcher $scopeMatcher
+    ) {}
 
     protected function getResponse(Template $template, ContentModel $contentModel, Request $request): Response
     {
@@ -32,7 +35,7 @@ class PageTeasersController extends AbstractContentElementController
             $arrPages = [];
             foreach ($ids as $id) {
                 $pageCollection = PageModel::findByPid($id, ['includeRoot' => true]);
-                \array_push($arrPages, ...$pageCollection->getModels());
+                array_push($arrPages, ...$pageCollection->getModels());
             }
 
             $pageModels = new Collection($arrPages, 'tl_page');
@@ -41,13 +44,21 @@ class PageTeasersController extends AbstractContentElementController
             $pageModels = PageModel::findPublishedRegularByIds($ids, ['includeRoot' => true]);
         }
 
-
         // Return if there are no pages
         if (!$pageModels instanceof Collection) {
             return new Response();
         }
 
-        $template->navigationTpl = $contentModel->navigationTpl ?: 'nav_teaser';
+        $template->navigationTpl = 'nav_teaser';
+
+        if ($contentModel->navigationTpl) {
+            $request = $this->requestStack->getCurrentRequest();
+
+            // Use the custom template unless it is a back end request
+            if (!$request || !$this->scopeMatcher->isBackendRequest($request)) {
+                $template->navigationTpl = $contentModel->navigationTpl;
+            }
+        }
 
         $currentPage = $this->getPageModel();
 
